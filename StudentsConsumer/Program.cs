290ec77e75d9c537c2bd4ed8138ed8580f7a3c35
Klolensen <31515047+Klolensen@users.noreply.github.com>
2018-02-12 13:38:03 +0100

    Created the consumer, still doesn't work though

--- StudentsConsumer/Program.cs
+++ StudentsConsumer/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StudentsConsumer.StudentService;
 
 namespace StudentsConsumer
 {
@@ -10,6 +11,28 @@ namespace StudentsConsumer
     {
         static void Main(string[] args)
         {
+            using (var client = new StudentService.Service1Client())
+            {
+                client.AddStudent("Kevin Jensen", "Program", "E202");
+                Console.WriteLine(client.GetAllStudent().Length);
+                foreach (var student in client.GetAllStudent())
+                {
+                    Console.WriteLine(student);
+                }
+                
+            }
+
+            //using (var client = new StudentService.Service1Client())
+            //{   
+            //    client.AddStudent("Kevin Jensen", "Program" , "E202");
+            //    string student = client.FindStudents("Kevin Jensen");
+            //    Console.WriteLine(student);
+            //    client.EditStudent("Kevin Jensen", "John Doe", "Technology", "E202");
+            //    Console.WriteLine(client.FindStudents("John Doe"));
+            //    client.RemoveStudent("John Doe");
+            //    Console.WriteLine(client.GetAllStudent().Length);
+            //}
+            Console.ReadKey();
         }
     }
 }
